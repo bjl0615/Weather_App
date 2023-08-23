1db2037df2bfce760d1ce83b868be8948fa72f40
admin <bjl0615@gmail.com>
2023-08-23 23:49:51 +0900

    Weather_App Data load part_1

---
+++ app/src/main/java/com/example/weather_app/Forecast.kt
@@ -0,0 +1,11 @@
+package com.example.weather_app
+
+data class Forecast (
+    val forecastDate : String,
+    val forecastTime : String,
+
+    var temperature : Double = 0.0,
+    var sky : String = "",
+    var precipitation : Int = 0,
+    var precipitationType : String = ""
+    )
